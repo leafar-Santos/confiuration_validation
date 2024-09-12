e8535332a7f0a2d6cff5e0ef5c415c9036cf2ae6
leafar-Santos <leafarccomputacao@gmail.com>
2024-09-12 11:48:00 -0300

    Ajuste jenkinsfile

--- src/main/java/br/com/serasa/suites/knnarSuite.java
+++ src/main/java/br/com/serasa/suites/knnarSuite.java
@@ -5,8 +5,11 @@ import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 
+/*
 @RunWith(JUnitPlatform.class)
 @SelectClasses({KnnarParameteresTest.class})
 public class knnarSuite {
 
 } ;
+
+*/
